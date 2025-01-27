4a5cfcdf418ae055c0efb0b65ced51b4dda00d60
Axel <Axel@DESKTOP-9QARTN3>
2025-01-27 18:02:59 +0100

    Add project files.

---
+++ AxelSchubertDB/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AxelSchubertDB.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Occupation { get; set; } = null!;
+
+    public string? ContactInfo { get; set; }
+
+    public int? Salary { get; set; }
+    
+    public DateOnly? HireDate { get; set; }
+
+    public virtual ICollection<Course> Courses { get; set; } = new List<Course>();
+}
